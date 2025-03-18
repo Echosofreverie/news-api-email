f39d562722cc6b0bee724801b91a74ce265ba82a
AnnaLIU-8987 <anliu6-C@my.cityu.edu.hk>
2025-03-18 22:24:47 +0800

    3/18: Send emails.

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from email.mime.text import MIMEText
 
 from pyexpat.errors import messages
 
-api_key = "792......"  #去https://newsapi.org/登录使用自己的API
+api_key = "7926374a804d45dc9ea439b6251f17ef"  #去https://newsapi.org/登录使用自己的API
 url = ("https://newsapi.org/v2/everything?q=tesla&"\
        "from=2025-02-18&sortBy=publishedAt&apiKey="\
-       "7.....")
+       "7926374a804d45dc9ea439b6251f17ef")
 # Make request
 request = requests.get(url)
 
@@ -18,18 +18,18 @@ print(content)
 body = " "
 for article in content["articles"]:
     if article["title"] is not None and article["description"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + "\n"
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
 
 
 def send_message(message):
-    qq_email = "22xxxx@qq.com"
-    auth_code = "xfxxxx"
+    qq_email = "2244089290@qq.com"
+    auth_code = "xfchtrwoqtofdidh"
     smtp_server = "smtp.qq.com"
     smtp_port = 465
     msg = MIMEText(message)
     msg['Subject'] = "New Email of News"
     msg['From'] = qq_email
-    msg['To'] = "22xxx90@qq.com"
+    msg['To'] = "2244089290@qq.com"
 
     try:
         # 创建SSL加密连接
