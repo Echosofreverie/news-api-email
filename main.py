a3d21f9f7dbca649f151a92ba526558343387770
AnnaLIU-8987 <anliu6-C@my.cityu.edu.hk>
2025-03-21 16:47:46 +0800

    3/21:  Limit the language and volume of news

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import smtplib
 from email.mime.text import MIMEText
 
 from pyexpat.errors import messages
-
-api_key = "7926374a804d45dc9ea439b6251f17ef"  #去https://newsapi.org/登录使用自己的API
-url = ("https://newsapi.org/v2/everything?q=tesla&"\
-       "from=2025-02-18&sortBy=publishedAt&apiKey="\
-       "7926374a804d45dc9ea439b6251f17ef")
+topic = "tesla"
+api_key = "79263...."  #去https://newsapi.org/登录使用自己的API
+url = (f"https://newsapi.org/v2/everything?"
+       f"q={topic}&"
+       "from=2025-02-21&sortBy=publishedAt&"
+       "apiKey=7926374a8.....&"
+       "language=en")
 # Make request
 request = requests.get(url)
 
@@ -16,20 +18,20 @@ content = request.json()
 print(content)
 # Get a list of articles
 body = " "
-for article in content["articles"]:
-    if article["title"] is not None and article["description"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+for article in content["articles"][:20]:
+    if article["title"] is not None and article["description"] is not None and article["url"] is not None:
+        body = body + article["title"] + "\n" + article["description"] +"\n"+article["url"]+ 2*"\n"
 
 
 def send_message(message):
-    qq_email = "2244089290@qq.com"
-    auth_code = "xfchtrwoqtofdidh"
+    qq_email = "2....@qq.com"
+    auth_code = "xfc...."
     smtp_server = "smtp.qq.com"
     smtp_port = 465
     msg = MIMEText(message)
-    msg['Subject'] = "New Email of News"
+    msg['Subject'] = "Today's news"
     msg['From'] = qq_email
-    msg['To'] = "2244089290@qq.com"
+    msg['To'] = "2.....@qq.com"
 
     try:
         # 创建SSL加密连接
